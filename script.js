92a823c946396e9cffa385b2ea590b5b13f7cb0f
Isaque Pereira <isaquepereirac@gmail.com>
2025-09-29 19:27:07 -0300

    Added micro interaction on refresh button

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 import contentLibrary from "./mantras.js";
 
-// let piece = document.querySelector('.piece');
-// let author = document.querySelector('.author')
-// let origin = document.querySelector('.origin')
-
-// let N = parseInt(Math.random() * contentLibrary.length);
-// console.log(N);
-// piece.innerText = contentLibrary[N].piece;
-// author.innerText = contentLibrary[N].author;
-// origin.innerText = contentLibrary[N].origin;
-
 function getRandomMantra() {
     const mantra = contentLibrary[Math.floor(Math.random() * contentLibrary.length)];
     console.log('Selected mantra:', mantra);
@@ -27,6 +17,7 @@ function displayMantra({piece, author, origin}) {
 
 displayMantra(getRandomMantra())
 
+// Refresh Button
 const btnRefresh = document.getElementById("btnRefresh");
 console.log(btnRefresh);
 
